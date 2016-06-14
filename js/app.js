00a4f169097565d1ee54fbd4d1bbbb4b46858994
anoopmundathan <anoop.mundathan@gmail.com>
2016-06-14 04:31:42 +0100

    Add img

--- js/app.js
+++ js/app.js
@@ -2,18 +2,38 @@ var app = app || {};
 $(document).ready(function() {
 	var bookList = [
 	{
+		coverImage: 'img/placeholder.png',
 		title:'JavaScript Good Parts',
 		author: 'Cockford'
 	},
 	{
+		coverImage: 'img/placeholder.png',
 		title: 'Backbone.js',
 		author: 'Addyosmani'
 	},
 	{
+		coverImage: 'img/placeholder.png',
 		title: 'Angular.js',
 		author: 'Paul'
+	}, 
+
+	{
+		coverImage: 'img/placeholder.png',
+		title: 'Backbone',
+		author: 'Addy',
+		releaseDate: 'Unknown',
+		keywords: 'Unknown'
+	},
+
+	{
+		coverImage: 'img/placeholder.png',
+		title: 'Backbone',
+		author: 'Anoop',
+		releaseDate: 'Unknown',
+		keywords: 'Unknown'
 	}
 
 	];
 	new app.LibraryView(bookList);
+	
 });
